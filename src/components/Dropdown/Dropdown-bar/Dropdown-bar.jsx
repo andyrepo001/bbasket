e84168a786759b700ec5c822a5fe8555a617fd7d
tayef mahmud <mahmudovi183@gmail.com>
2024-05-08 12:00:30 +0600

    multi dropdown

--- src/components/Dropdown/Dropdown-bar/Dropdown-bar.jsx
+++ src/components/Dropdown/Dropdown-bar/Dropdown-bar.jsx
@@ -4,20 +4,30 @@ import demo from "@/assets/icon_baby-products.png";
 import arrow from "@/assets/arrow.png";
 import Image from "next/image";
 import { useFetch } from "@/lib/use-client-side-data";
+import SubMenu from "../Sub-menu/Sub-menu";
+import { useState } from "react";
 
 export default function DropdownBar() {
   const categories = useFetch("item_categories");
+  const [selectedSubmenu, setSelectedSubenu] = useState(null);
+
   return (
     <div className={styles.dropdown_bar}>
       <div className={styles.dropdown_bar_wrapper}>
         {categories?.data?.map((category, indx) => (
-          <div className={styles.dropdown_item} key={indx}>
-            <Icon src={demo} />
-            <h4 className={styles.dropdown_item_text}>
-              {category?.cat_name_en}
-            </h4>
-            <Image src={arrow} alt="arrow" className={styles.dropdown_icon} />
-          </div>
+          <>
+            <div
+              className={styles.dropdown_item}
+              key={indx}
+              onMouseEnter={() => setSelectedSubenu(category?.cat_id)}
+            >
+              <Icon src={demo} />
+              <h4 className={styles.dropdown_item_text}>
+                {category?.cat_name_en}
+              </h4>
+              <Image src={arrow} alt="arrow" className={styles.dropdown_icon} />
+            </div>
+          </>
         ))}
       </div>
     </div>
--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,12 +1,37 @@
 "use client";
 import styles from "./Dropdown.module.css";
+import { useFetch } from "@/lib/use-client-side-data";
+import { useState } from "react";
+import { useDropdown } from "@/hooks/use-dropdown";
 import Icon from "../Icon/Icon";
 import demo from "@/assets/groceries.png";
-import DropdownBar from "./Dropdown-bar/Dropdown-bar";
-import { useDropdown } from "@/hooks/use-dropdown";
+import arrow from "@/assets/arrow.png";
+import Image from "next/image";
 
 export default function Dropdown() {
+  const categories = useFetch("item_categories");
   const dropdown = useDropdown();
+  const [hoveredSubmenu, setHoveredSubmenu] = useState(null);
+
+  const RenderSubmenu = ({ submenu }) => {
+    return (
+      <>
+        {submenu?.child && (
+          <div
+            className={`${
+              hoveredSubmenu === submenu?.cat_id
+                ? styles.submenu
+                : styles.hidden
+            }`}
+          >
+            {submenu?.child?.map((item) => (
+              <span key={item.cat_id}>{item.cat_name_en}</span>
+            ))}
+          </div>
+        )}
+      </>
+    );
+  };
 
   return (
     <div
@@ -19,7 +44,32 @@ export default function Dropdown() {
           What do you want to buy today?
         </h1>
       </div>
-      {dropdown.isOpen && <DropdownBar />}
+      {dropdown.isOpen && (
+        <div className={styles.dropdown_bar}>
+          <div className={styles.dropdown_bar_wrapper}>
+            {categories?.data?.map((category, indx) => (
+              <div
+                className={styles.dropdown_item}
+                key={indx}
+                onMouseEnter={() => setHoveredSubmenu(category?.cat_id)}
+                onMouseLeave={() => setHoveredSubmenu(null)}
+              >
+                <Icon src={demo} />
+                <h4 className={styles.dropdown_item_text}>
+                  {category?.cat_name_en}
+                </h4>
+                <Image
+                  src={arrow}
+                  alt="arrow"
+                  className={styles.dropdown_icon}
+                />
+
+                <RenderSubmenu submenu={category} />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
